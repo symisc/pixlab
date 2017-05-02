d35f3f8231085aeaee7121790d21327eedf52ca5
Symisc Systems <chm@symisc.net>
2017-05-03 01:50:30 +0200

    Censure all human faces present in a given image

---
+++ PHP/censure_human_faces.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * PixLab PHP Client which is just a single class PHP file without any dependency that you can get from Github
+ * https://github.com/symisc/pixlab-php 
+ */
+require_once "pixlab.php";
+
+
+/* Censure all human faces present in a given image by invoking 'facedetect' first which return the rectangle coordinates 
+ * of all human faces present & then invoke 'mogrify' to censure those faces.
+ */ 
+
+# Target Image: Feel free to change to whatever image you want
+$img = 'https://i.redd.it/oetdn9wc13by.jpg';
+# Your PixLab key
+$key = 'My_Pix_Key';
+
+$pix = new Pixlab($key);
+echo "Detecting faces first...\n";
+/* Invoke facedetect first  */
+if( !$pix->get('facedetect',array('img' => $img)) ){
+	echo $pix->get_error_message();
+	die;
+}
+/* Grab the total number of detected faces */
+$faces = $pix->json->faces;
+echo "Total number of detected faces: ".count($faces)."\n";
+
+if( count($faces) < 1 ){
+	echo "No human faces were were detected on this picture\n";
+}else{
+	echo "Censuring faces...\n";
+	/* Call mogrify (Only POST) */
+	if( !$pix->post('mogrify',array('img' => $img,'key'=> $key,'cord' => $faces)) ){
+		echo $pix->get_error_message();
+	}else{
+		echo "Censured Faces: ".$pix->json->link."\n";
+	}
+}
+?>
