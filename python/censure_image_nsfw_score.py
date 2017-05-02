ae2c083cf80bddb3fcbdee5ae72258ee2dd74cbe
Symisc Systems <chm@symisc.net>
2017-05-02 02:50:21 +0200

    Censure Images based on their on NSFW score

---
+++ python/censure_image_nsfw_score.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+# Target Image: Change to any link (Possibly adult) you want or switch to POST if you want to upload your image directly, refer to the sample set for more info.
+img = 'https://i.imgur.com/roMrdr1.png' 
+# Your PixLab key
+key = 'Pixlab_Key'
+
+# Censure an image based on its NSFW score
+req = requests.get('https://api.pixlab.io/nsfw',params={'img':img,'key':key})
+reply = req.json()
+if reply['status'] != 200:
+	print (reply['error'])
+elif reply['score'] < 0.5 :
+    print ("No adult content were detected on this picture")
+else:
+    # Highly NSFW picture
+	print ("Censuring NSFW picture...")
+	req = requests.get('https://api.pixlab.io/blur',params={'img':img,'key':key,'rad':50,'sig':30})
+	reply = req.json()
+	if reply['status'] != 200:
+		print (reply['error'])
+	else:
+		print ("Censured image: "+ reply['link'])
