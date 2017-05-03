e0ff328b9c31b2326d1dea04e10c85dc46a80005
Symisc Systems <chm@symisc.net>
2017-05-03 03:01:04 +0200

    Update screen_capture.py

--- python/screen_capture.py
+++ python/screen_capture.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-# Extarct a screen capture from a given website
+# Extract a screen capture from a given website
 website = 'https://github.com'
 
 key = 'My_PixLab_Key'
 
-req = requests.get('https://api.pixlab.io/screencapture',params={'url':website,'key':key}) # You can optionally, speciy the desired width & height also
+req = requests.get('https://api.pixlab.io/screencapture',params={'url':website,'key':key}) # Specify the desired width & height if you want to
 reply = req.json()
 if reply['status'] != 200:
 	print (reply['error'])
