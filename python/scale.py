0642b3c2b93850239edbc231a7eba08287357f10
Symisc Systems <chm@symisc.net>
2017-05-04 03:04:14 +0200

    Resize an image to half its original size

---
+++ python/scale.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+# Resize an image to half its original size hence the 50% field.
+
+import requests
+import json
+
+req = requests.get('https://api.pixlab.io/scale',params={
+	'img': 'https://www.allaboutbirds.org/guide/PHOTO/LARGE/blue_jay_8.jpg',
+	'key':'My_Pix_Key',
+	"scale":50
+})
+reply = req.json()
+if reply['status'] != 200:
+    print (reply['error'])
+else:
+    print ("Pic location: "+ reply['link'])
