6c86e6c4457205c56e586e3943a1b81415b868d6
Symisc Systems <chm@symisc.net>
2017-05-04 02:10:24 +0200

    OCR: Detect input language & extract text content from there

---
+++ python/ocr.py
@@ -0,0 +1,11 @@
+import requests
+import json
+# Given an image with human readable characters. Detect input language & extract text content from there.
+# https://pixlab.io/#/cmd?id=ocr for additional information.
+req = requests.get('https://api.pixlab.io/ocr',params={'img':'http://quotesten.com/wp-content/uploads/2016/06/Confucius-Quote.jpg','key':'My_PixLab_Key'})
+reply = req.json()
+if reply['status'] != 200:
+	print (reply['error'])
+else:
+	print ("Input language: " + reply['lang'])
+	print ("Text Output: " + reply['output'])
