641e31fcb05016e5fb27e8f115a763b72c7638f3
Symisc Systems <chm@symisc.net>
2017-05-06 02:32:24 +0200

    Tag an image based on detected visual content

--- python/tag_img.py
+++ python/tag_img.py
@@ -9,7 +9,6 @@ img = 'https://s-media-cache-ak0.pinimg.com/originals/35/d0/f6/35d0f6ee0e40306c4
 # Your PixLab key
 key = 'My_PixLab_Key'
 
-# Censure an image based on its NSFW score
 req = requests.get('https://api.pixlab.io/tagimg',params={'img':img,'key':key})
 reply = req.json()
 if reply['status'] != 200:
