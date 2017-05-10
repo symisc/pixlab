ca97044fac8bfabee437b6791f052a4ac07e819d
Symisc Systems <chm@symisc.net>
2017-05-11 01:47:29 +0200

    Blur & Censure picture

---
+++ PHP/blur.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * PixLab PHP Client which is just a single class PHP file without any dependency that you can get from Github
+ * https://github.com/symisc/pixlab-php 
+ */
+require_once "pixlab.php";
+/*
+ * Blur the bird picture
+ * Refer to https://pixlab.io/#/cmd?id=blur for more info.
+ */
+# Your PixLab key
+$key = 'My_Pixlab_Key';
+
+$pix = new Pixlab($key);
+if( !$pix->get('blur',array(
+		'img'=>'https://www.allaboutbirds.org/guide/PHOTO/LARGE/blue_jay_8.jpg',
+		'radius'=>50,
+		'sigma'=>30,
+		'key'=>$key
+	))){
+	echo $pix->get_error_message();
+	die;
+}
+echo "Pic Link: ".$pix->json->link."\n";
+
+?>
