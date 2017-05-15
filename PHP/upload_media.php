8646d8205cad6af53f952630f8655c98dce91904
Symisc Systems <chm@symisc.net>
2017-05-15 04:05:01 +0200

    Upload a local image
     Upload a local image to the remote https://pixlab.xyz storage server or your own S3 bucket depending on the configuration from your dashboard.
    Use the output link for other purposes such as processing via mogrify, drawrectangles, etc. or simply serving content.
    https://pixlab.io/#/cmd?id=store for more info.

---
+++ PHP/upload_media.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * PixLab PHP Client which is just a single class PHP file without any dependency that you can get from Github
+ * https://github.com/symisc/pixlab-php 
+ */
+require_once "pixlab.php";
+
+# Upload a local image to the remote https://pixlab.xyz storage server or your own S3 bucket depending on the configuration from your dashboard.
+# Use the output link for other purposes such as processing via mogrify, drawrectangles, etc. or simply serving content.
+# https://pixlab.io/#/cmd?id=store for more info.
+
+$img_path = './loca_file.png';
+
+/* Process */
+$pix = new Pixlab('My_Pix_Key');
+
+$pix->switch_to_http();  /* Switch to http for fast upload */
+
+if( !$pix->post('store',array('comment' => 'Super Secret Image'),$img_path) ){
+	echo $pix->get_error_message()."\n";
+	die;
+}
+echo "Uploaded Pic Link: ".$pix->json->link."\n";
