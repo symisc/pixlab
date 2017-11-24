676dd9ce6f5284c07884fa0015b65ad2dfdce8fa
Symisc Systems <chm@symisc.net>
2017-11-24 02:03:48 +0100

    Composite a static image on top of a GIF

--- PHP/gif_composite.php
+++ PHP/gif_composite.php
@@ -5,8 +5,8 @@
  */
 require_once "pixlab.php";
 
-# Composite a static image on top of a this GIF which should be displayed starting from the frame number 5
-# If you want to composite multiple images, the use the 'merge' command instead.
+# Composite a static image on top of a GIF.
+# If you want to composite multiple images, then use the `merge` command instead.
 
 # https://pixlab.io/#/cmd?id=gifcomposite
 
@@ -19,10 +19,10 @@ $pix = new Pixlab('My_Pix_Key');
 if( !$pix->get('gifcomposite',[
 	'img' => $gif,
 	'composite' => $static,
-    'x' => 10,
+	'x' => 10,
 	'y' => 30,
 	'frame' => 5, #Display the result starting from frame number 5
-	]) ){
+]) ){
 	echo $pix->get_error_message()."\n";
 	die;
 }
