ea34a7a6934ccbb3e60a4c5361b3fc6b6cb792b7
Symisc Systems <chm@symisc.net>
2020-05-31 18:04:03 +0200

    Apply a blur filter automatically for each detected face

--- PHP/blur_human_faces.php
+++ PHP/blur_human_faces.php
@@ -6,12 +6,12 @@
 require_once "pixlab.php";
 
 # Detect all human faces in a given image via `facedetect` and blur all of them via `mogrify`.
-# https://pixlab.io/#/cmd?id=facedetect & https://pixlab.io/#/cmd?id=mogrify for additional information.
+# https://pixlab.io/cmd?id=facedetect & https://pixlab.io/cmd?id=mogrify for additional information.
 
-# Target Image: Feel free to change to whatever image you want
-$img = 'http://cf.broadsheet.ie/wp-content/uploads/2015/03/jeremy-clarkson_3090507b.jpg';
-# Your PixLab key
-$key = 'My_Pix_Key';
+# Target Image we want to blur face(s) on
+$img = 'https://pixlab.io/images/m3.jpg';
+# Your PixLab API key
+$key = 'PIXLAB_API_KEY';
 
 $pix = new Pixlab($key);
 echo "Detecting faces first...\n";
@@ -27,12 +27,12 @@ echo "Total number of detected faces: ".count($faces)."\n";
 if( count($faces) < 1 ){
 	echo "No human faces were were detected on this picture\n";
 }else{
-	echo "Censuring faces...\n";
+	echo "Blurring faces...\n";
 	/* Call mogrify (Only POST) */
-	if( !$pix->post('mogrify',array('img' => $img,'cord' => $faces)) ){
+	if( !$pix->post('mogrify', ['img' => $img,'cord' => $faces]) ){
 		echo $pix->get_error_message();
 	}else{
-		echo "Censured Faces: ".$pix->json->link."\n";
+		echo "Blurred faces URL: ".$pix->json->link."\n";
 	}
 }
 ?>
