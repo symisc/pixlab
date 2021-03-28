39f429ad6c978f56dd2e6d204d77a8c9f588b12f
PixLab | Symisc Systems <chm@symisc.net>
2021-03-28 04:14:55 +0200

    Upload a local media file using the PixLab API

--- python/upload_media.py
+++ python/upload_media.py
@@ -1,15 +1,15 @@
 import requests
 import json
 
-# Upload a local image to the remote https://pixlab.xyz storage server or your own S3 bucket depending on the configuration from your dashboard.
+# Upload a local media file to the remote https://pixlab.xyz storage server or your own S3 bucket depending on the configuration on your PixLab dashboard.
 # Use the output link for other purposes such as processing via mogrify, drawrectangles, etc. or simply serving content.
-# https://pixlab.io/#/cmd?id=store for more info.
+# https://pixlab.io/cmd?id=store for more info.
 
 req = requests.post('http://api.pixlab.io/store', # Switch to http:// for fast upload
 	files = {'file': open('./local_image.png', 'rb')},
 	data={
 		'comment':'Super Secret Stuff',
-		'key':'My_Pix_Key',
+		'key': 'PIXLAB_API_KEY' # PixLab API Key - Get yours from https://pixlab.io/dashboard
 	}
 )
 reply = req.json()
