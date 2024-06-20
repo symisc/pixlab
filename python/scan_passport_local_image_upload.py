a66bb59aee7832be1be4eeca3562bca3b69da844
PixLab | Symisc Systems <chm@symisc.net>
2024-06-20 11:11:36 +0100

    Local ID Document Upload to DOCSCAN - https://ekyc.pixlab.io/docscan

--- python/scan_passport_local_image_upload.py
+++ python/scan_passport_local_image_upload.py
@@ -13,29 +13,31 @@ import json
 #
 # Refer to the official documentation at: https://ekyc.pixlab.io/docscan for the API reference guide and more code samples.
 
-req = requests.get(
-	'https://api.pixlab.io/docscan', 
-	params={
-		'img':'https://i.stack.imgur.com/oJY2K.png', # Passport Input Image
-		'type':'passport', # Type of document we are a going to scan
-		'key':'PIXLAB_API_KEY' # Get your PixLab API Key from https://console.pixlab.io
+req = requests.post(
+	'https://api.pixlab.io/docscan',
+	files = {
+		'file': open('./local_passport_image.png', 'rb') # The local Passport image we are going to send to DOCSCAN
+	},
+	data={
+		'type':'passport', # Type of document we are a going to scan,
+		'key':'PIXLAB_API_KEY' # PixLab API Key - Get yours from https://console.pixlab.io/'
 	}
 )
 reply = req.json()
 if reply['status'] != 200:
-	print (reply['error'])
+  print (reply['error'])
 else:
-	print ("User Cropped Face: " + reply['face_url'])
-	print ("Raw MRZ Text: " + reply['mrz_raw_text'])
-	print ("Extracted Passport MRZ Fields:")
-	# Display all extracted MRZ fields
-	print ("\tIssuing Country: " + reply['fields']['issuingCountry'])
-	print ("\tFull Name: "       + reply['fields']['fullName'])
-	print ("\tDocument Number: " + reply['fields']['documentNumber'])
-	print ("\tCheck Digit: "   + reply['fields']['checkDigit'])
-	print ("\tNationality: "   + reply['fields']['nationality'])
-	print ("\tDate Of Birth: " + reply['fields']['dateOfBirth'])
-	print ("\tSex: "           + reply['fields']['sex'])
-	print ("\tDate Of Expiry: "    + reply['fields']['dateOfExpiry'])
-	print ("\tPersonal Number: "   + reply['fields']['personalNumber']) # Optional field and may not be returned when not set by the issuing country.
-	print ("\tFinal Check Digit: " + reply['fields']['finalcheckDigit'])
+  print ("User Cropped Face: " + reply['face_url'])
+  print ("Raw MRZ Text: " + reply['mrz_raw_text'])
+  print ("Extracted Passport MRZ Fields:")
+  # Display all extracted MRZ fields
+  print ("\tIssuing Country: " + reply['fields']['issuingCountry'])
+  print ("\tFull Name: "       + reply['fields']['fullName'])
+  print ("\tDocument Number: " + reply['fields']['documentNumber'])
+  print ("\tCheck Digit: "   + reply['fields']['checkDigit'])
+  print ("\tNationality: "   + reply['fields']['nationality'])
+  print ("\tDate Of Birth: " + reply['fields']['dateOfBirth'])
+  print ("\tSex: "           + reply['fields']['sex'])
+  print ("\tDate Of Expiry: "    + reply['fields']['dateOfExpiry'])
+  print ("\tPersonal Number: "   + reply['fields']['personalNumber'])
+  print ("\tFinal Check Digit: " + reply['fields']['finalcheckDigit'])
