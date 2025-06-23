4c4a8ef57bdcec548f5c1b4a38f798340d5dc15c
PixLab | Symisc Systems <chm@symisc.net>
2025-06-23 01:44:27 +0100

    Create generate_image_description.php

---
+++ PHP/generate_image_description.php
@@ -0,0 +1,22 @@
+<?php
+# Generate a natural language description of an image content
+
+# Target Image: Change to any link or switch to POST if you want to upload your image directly, refer to the REST API code samples for more info.
+$img = 'https://s-media-cache-ak0.pinimg.com/originals/35/d0/f6/35d0f6ee0e40306c41cfd714c625f78e.jpg';
+
+$key = 'PIXLAB_API_KEY'; // Get your API key from https://console.pixlab.io/
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://api.pixlab.io/describe?img=' . urlencode($img) . '&key=' . $key . '&lang=english&short=false');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$reply = json_decode(curl_exec($ch), true);
+curl_close($ch);
+
+if ($reply['status'] != 200) {
+    echo $reply['error'];
+} else {
+    $description = $reply['description'];
+    echo "Natural language content description: " . $description;
+}
