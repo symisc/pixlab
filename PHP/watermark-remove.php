53a6e4ef4125830439d000bf2664c1351414ffdd
PixLab | Symisc Systems <chm@symisc.net>
2025-10-30 18:40:48 +0100

    Add HTTP header for multipart form data

--- PHP/watermark-remove.php
+++ PHP/watermark-remove.php
@@ -23,6 +23,7 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: multipart/form-data"));
 
 $response = curl_exec($ch);
 curl_close($ch);
